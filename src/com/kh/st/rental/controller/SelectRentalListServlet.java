e571b9cea65942c81c2060fd8480bff62c754cfd
uukk <uukk@DESKTOP-KG3LVV9>
2019-05-14 21:09:42 +0900

    조회필터 작업중

--- src/com/kh/st/rental/controller/SelectRentalListServlet.java
+++ src/com/kh/st/rental/controller/SelectRentalListServlet.java
@@ -38,7 +38,7 @@ public class SelectRentalListServlet extends HttpServlet {
 			request.setAttribute("list", list);
 		}
 		request.getRequestDispatcher(page).forward(request, response);
-
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
