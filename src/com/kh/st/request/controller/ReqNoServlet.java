689b81e9d2dbbf4ed3f87811a72a0d02b1bf9589
USER <USER@DESKTOP-DVQO56D>
2019-05-15 21:18:09 +0900

    등록 거절 모달 리스트 추가 구현

---
+++ src/com/kh/st/request/controller/ReqNoServlet.java
@@ -0,0 +1,44 @@
+package com.kh.st.request.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReqNoServlet
+ */
+@WebServlet("/reqNo.bo")
+public class ReqNoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReqNoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		
+		String textResult = request.getParameter("textResult");
+		
+		System.out.println(textResult);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
